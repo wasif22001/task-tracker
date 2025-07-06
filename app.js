452ecc1ff49ce6a467d57de2bf282e87e51a4461
wasif ali <wasif.shah@d-p-s.com>
2025-07-06 23:47:24 +0800

    Configured Swagger

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const taskRoutes = require("./routes/taskRoutes");
+const connectDB = require("./config/db");
+
+const { swaggerUi, swaggerSpec } = require("./docs/swagger");
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Task Tracker API");
 });
 
-app.use("/api/tasks", taskRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  });
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
